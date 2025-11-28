e2a8187e9e9e11d4d017fa7b7cc8714457238d46
Rabiu Muhammad <mrabiu321@gmail.com>
2025-11-28 17:54:05 +0100

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Build from '@/components/Build';
 import ContactForm from '@/components/ContactForm';
 import Testimonials from '@/components/Testimonials';
 
@@ -5,6 +6,7 @@ const HomePage = () => {
   return (
     <div className="min-h-screen">
       <main>
+        <Build />
         <Testimonials />
         <ContactForm />
       </main>
