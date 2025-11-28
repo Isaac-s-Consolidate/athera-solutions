17f15226df0ce51bedcf594523dfa1d9bbafdca1
profe-ssor <kyerematengcollins93@gmail.com>
2025-11-28 09:19:36 +0000

    responsiveness and Animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ContactForm from '@/components/ContactForm';
+import WhoWeAre from '@/components/WhoWeAre';
+import OurServices from '@/components/OurServices';
 
 const HomePage = () => {
   return (
     <div className="min-h-screen">
       <main>
+        <WhoWeAre />
+        <OurServices />
         <ContactForm />
       </main>
     </div>
