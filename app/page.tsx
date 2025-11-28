236e4fb1d0ba6b645fe44582d58d7c2e136d8776
aryanraj-01 <2aryan4monu@gmail.com>
2025-11-28 21:35:59 +0530

    Hero_section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Hero from '@/components/Hero';
 import ContactForm from '@/components/ContactForm';
 
 const HomePage = () => {
   return (
     <div className="min-h-screen">
       <main>
+        <Hero />
         <ContactForm />
       </main>
     </div>
