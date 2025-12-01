6ce017344502f0326b9d5d2b885f200c4c3db1be
Isaacs Consolidate <isaacconsolidate@gmail.com>
2025-12-01 02:48:47 +0000

    Dev (#15)
    * [ATH-12] Contact Page (#9)
    
    * feat(ATH-11): BuildFastTestimonial & Delivering (#12)
    
    * feat(ATH-11): initial commit
    
    * fix(TESTIMONIALS-UI): escape quotes in testimonial text to satisfy ESLint
    
    * Update Testimonials.tsx
    
    * fix(typescript): add module declarations for image imports
    
    * Responsiveness of Testimonial
    
    * Hero_section
    
    * ATH - Services (#18)
    
    * feat(ATH-11): initial commit
    
    * fix(TESTIMONIALS-UI): escape quotes in testimonial text to satisfy ESLint
    
    * Update Testimonials.tsx
    
    * fix(typescript): add module declarations for image imports
    
    * feat: initial commit
    
    * feat(ui): added cursor to cards
    
    * feat: initial commit
    
    * fix(eslint): fixed
    
    * chore: format code and fix lint issues
    
    * ATH-11-(BuildFastTestimonial & Delivering) (#17)
    
    * feat(ATH-11): initial commit
    
    * fix(TESTIMONIALS-UI): escape quotes in testimonial text to satisfy ESLint
    
    * Update Testimonials.tsx
    
    * fix(typescript): add module declarations for image imports
    
    * feat: initial commit
    
    * feat(ui): added cursor to cards
    
    ---------
    
    Co-authored-by: profe-ssor <87576786+profe-ssor@users.noreply.github.com>
    Co-authored-by: Rabiu Muhammad <111989601+DarkVayder@users.noreply.github.com>
    Co-authored-by: profe-ssor <kyerematengcollins93@gmail.com>
    Co-authored-by: aryanraj-01 <2aryan4monu@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import Build from '@/components/Build';
 import ContactForm from '@/components/ContactForm';
+import Hero from '@/components/Hero';
 import WhoWeAre from '@/components/WhoWeAre';
 import OurServices from '@/components/OurServices';
 import Testimonials from '@/components/Testimonials';
@@ -8,8 +9,10 @@ const HomePage = () => {
   return (
     <div className="min-h-screen">
       <main>
+        <Hero />
         <WhoWeAre />
         <OurServices />
+        <Build />
         <Testimonials />
         <ContactForm />
       </main>
