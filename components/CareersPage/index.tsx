9e315534200c37bb0669e9d752273f0900d434eb
Uche Obiekwe <obiekweuchenna@gmail.com>
2025-12-02 18:59:20 +0100

    feat: Add ImpactCard and ASButton components; integrate ContactForm into CareersPage

--- components/CareersPage/index.tsx
+++ components/CareersPage/index.tsx
@@ -4,6 +4,7 @@ import leftArrow from '@/app/assets/left.png';
 import rightArrow from '@/app/assets/right.png';
 import React from 'react';
 import { desc } from 'framer-motion/client';
+import ContactForm from '../ContactForm';
 
 // Reusable Components - can be extracted to separate files later---
 
@@ -22,6 +23,19 @@ interface NoCardSectionProps {
   header: string;
   description: string;
 }
+
+interface ASButtonProps {
+  text: string;
+  shadow?: boolean;
+}
+
+const ASButton = ({ text }: ASButtonProps) => {
+  return (
+    <button className="w-[377px] shrink-0 p-4 border border-[#E6E7E8] rounded-[30px] font-semibold md:text-[22px] text-[20px] hover:bg-[#0BB453] hover:border-[#0BB453] hover:shadow-md/50 transition duration-300">
+      {text}
+    </button>
+  );
+};
 
 const SectionHeader = ({ header }: { header: string }) => (
   <div className="mb-10 flex items-center justify-center">
@@ -99,6 +113,24 @@ const NoCardSection = ({ header, description }: NoCardSectionProps) => {
         <hr className="h-0.5 border-0 bg-linear-to-r from-[rgba(1,10,4,0.3)] via-[rgba(160,251,205,0.3)] to-[rgba(1,10,4,0.3)]" />
       </div>
     </section>
+  );
+};
+
+const ImpactCard = () => {
+  return (
+    <div className=" flex justify-center items-center mb-30 sm:px-10 px-50">
+      <div className="flex flex-col items-center justify-center max-w-[1200px] py-15  md:px-30 px-10 rounded-[20px] gap-8 text-[#E6E7E8] bg-[linear-gradient(94.87deg,#242243_24.19%,#0BB453_98.65%)]">
+        <div className="font-semibold md:text-[40px] text-[35px] text-center leading-[100%]">
+          Delivering <span className="text-[#0BB453]">Measurable Impact</span>
+        </div>
+        <div className="md:text-[24px] text-[20px] text-center">
+          Ready to transform your ideas into scalable reality? We deliver
+          end-to-end digital transformation with technical precision
+        </div>
+
+        <ASButton text="Start Your Project Today" />
+      </div>
+    </div>
   );
 };
 
@@ -222,13 +254,13 @@ const GrowthAndPerks = () => {
           We’re always looking for curious, creative, and driven minds. Ready to
           build the future with us?
         </div>
-        <button className="w-[377px] shrink-0 p-5  border border-[#E6E7E8] rounded-[30px] font-semibold text-[`22px] hover:bg-[#0BB453] hover:border-[#0BB453] transition duration-300">
-          Join our Team
-        </button>
+        <ASButton text="Join our Team" />
       </div>
     </section>
   );
 };
+
+// MAIN PAGE COMPONENT
 const CareersPage = () => {
   return (
     <>
@@ -237,6 +269,8 @@ const CareersPage = () => {
       <CareerOpportunities />
       <HowToApply />
       <GrowthAndPerks />
+      <ImpactCard />
+      <ContactForm />
     </>
   );
 };
